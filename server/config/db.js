6e45ed128101f0f14bf654cb51e0cf61b873978f
juanpablohp33 <juanpablohp31@gmail.com>
2022-06-25 03:56:00 -0500

    Change URL of database

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
-  //local MongoDB deployment ->
-  "URI": "mongodb://localhost/midterm229"
-  /*ADD your cloud URL Here, database Name should be midterm229, and collection name should be the one in the model*/
+    //local MongoDB deployment ->
+    // "URI": "mongodb://localhost/midterm229"
+    "URI": "mongodb+srv://juanpablohp31:examen123.@cluster0.irrkqhg.mongodb.net/midterm229"
+        /*ADD your cloud URL Here, database Name should be midterm229, and collection name should be the one in the model*/
 };
